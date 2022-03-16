c1d35485610a6f56f6f7f56214935c957dfd75bf
Hassanali Momin <hmomin2001@gmail.com>
2022-03-16 13:20:11 -0400

    lecture files

--- java/module-1/08_Collections_Part_2_Maps/lecture/src/main/java/com/techelevator/Lecture.java
+++ java/module-1/08_Collections_Part_2_Maps/lecture/src/main/java/com/techelevator/Lecture.java
@@ -13,6 +13,27 @@ public class Lecture {
 		System.out.println("####################");
 		System.out.println();
 
+		Map<String, String> animalGroups = new HashMap<>();
+
+		if (!animalGroups.containsKey(animalName.toLowerCase())) {
+			return "unknown";
+		} else {
+			return animalGroups.get(animalName.toLowerCase);
+		}
+
+
+		Map<Integer, Integer> output = new HashMap<>();
+
+		for (Integer i: ints) {
+			if (output.containsKey(i)){
+				output.put(i,output.get(i)+1);
+			}
+
+			if (!output.containsKey(i)){
+				output.put(i, 1);
+			}
+		}
+		return output;
 
 	}
 
