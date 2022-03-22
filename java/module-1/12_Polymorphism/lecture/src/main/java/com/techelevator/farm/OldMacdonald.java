d7b236604dfb6df8ba229ae66c7eab05c3f3c0c7
Hassanali Momin <hmomin2001@gmail.com>
2022-03-22 13:01:23 -0400

    lecture code

--- java/module-1/12_Polymorphism/lecture/src/main/java/com/techelevator/farm/OldMacdonald.java
+++ java/module-1/12_Polymorphism/lecture/src/main/java/com/techelevator/farm/OldMacdonald.java
@@ -3,11 +3,11 @@ package com.techelevator.farm;
 public class OldMacdonald {
 	public static void main(String[] args) {
 
-		FarmAnimal[] farmAnimals = new FarmAnimal[] { new Cow(), new Chicken() };
+		Singable[] singables = new Singable[] { new Cow(), new Chicken(), new Tractor() };
 
-		for (FarmAnimal animal : farmAnimals) {
-			String name = animal.getName();
-			String sound = animal.getSound();
+		for (Singable singable : singables) {
+			String name = singable.getName();
+			String sound = singable.getSound();
 			System.out.println("Old MacDonald had a farm, ee, ay, ee, ay, oh!");
 			System.out.println("And on his farm he had a " + name + ", ee, ay, ee, ay, oh!");
 			System.out.println("With a " + sound + " " + sound + " here");
