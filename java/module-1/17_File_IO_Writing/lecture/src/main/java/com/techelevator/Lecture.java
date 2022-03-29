e72bb04e5728958715c9dbc76bf5bb48dea929cf
Hassanali Momin <hmomin2001@gmail.com>
2022-03-29 13:06:14 -0400

    lecture code

--- java/module-1/17_File_IO_Writing/lecture/src/main/java/com/techelevator/Lecture.java
+++ java/module-1/17_File_IO_Writing/lecture/src/main/java/com/techelevator/Lecture.java
@@ -1,13 +1,14 @@
 package com.techelevator;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class Lecture {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args)  {
 
 		Scanner userInput = new Scanner(System.in);
 
@@ -19,6 +20,40 @@ public class Lecture {
 		 *
 		 * A new instance of File can be created from a String that contains a file system path
 		 */
+
+		File thisHereFile = new File ("google.txt");
+		String textFileData = "This is your file.\n this file is my file\nthis is for me";
+		String appendFileData = "\nIdk i wanna jus have a JoB";
+
+		FileTestingClass ftc = new FileTestingClass();
+
+		try {
+			ftc.createFile(thisHereFile);
+			ftc.writeData(thisHereFile, textFileData);
+			ftc.appendDataToFile(thisHereFile, appendFileData);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e){
+			e.getMessage();
+		}
+
+
+
+
+
+		File newDirectory = new File ("myDirectory");
+
+		if (newDirectory.exists()){
+			System.out.println("Directory already exists");
+		}else{
+			newDirectory.mkdir();
+		}
+
+//		File newFileInADirectory = new File("myDirectory", "myFileThatsInADirectory.txt");
+//
+//		newFileInADirectory.createNewFile();
+
+
 
 
 
