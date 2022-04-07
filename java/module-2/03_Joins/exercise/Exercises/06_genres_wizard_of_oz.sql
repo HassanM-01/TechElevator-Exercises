b75066f9532c301de0bc4b103c87faf57b668f63
Hassanali Momin <hmomin2001@gmail.com>
2022-04-07 08:01:31 -0400

    sql day 3 exercises

--- java/module-2/03_Joins/exercise/Exercises/06_genres_wizard_of_oz.sql
+++ java/module-2/03_Joins/exercise/Exercises/06_genres_wizard_of_oz.sql
@@ -1,3 +1,7 @@
 -- 6. The genres of "The Wizard of Oz" (3 rows)
-
+SELECT genre_name
+FROM movie
+JOIN movie_genre ON movie.movie_id = movie_genre.movie_id
+JOIN genre ON movie_genre.genre_id = genre.genre_id
+WHERE title = 'The Wizard of Oz'
 
--- java/module-2/03_Joins/exercise/Exercises/14_actors_back_to_the_future_collection.sql
+++ java/module-2/03_Joins/exercise/Exercises/14_actors_back_to_the_future_collection.sql
@@ -1,2 +1,7 @@
 -- 14. The names of actors who've appeared in the movies in the "Back to the Future Collection" (28 rows)
-
+SELECT DISTINCT person_name
+FROM movie
+JOIN movie_actor ON movie.movie_id = movie_actor.movie_id
+JOIN person ON person.person_id = movie_actor.actor_id
+JOIN collection ON collection.collection_id = movie.collection_id
+WHERE collection_name = 'Back to the Future Collection'
