774c2ac5ab9db921caa8fb7e2ef00a1ae196ecf2
Hassanali Momin <hmomin2001@gmail.com>
2022-04-20 15:49:53 -0400

    server side APIs

--- java/module-2/13_Server_Side_APIs_Part_1/exercise/src/main/java/com/techelevator/auctions/controller/AuctionController.java
+++ java/module-2/13_Server_Side_APIs_Part_1/exercise/src/main/java/com/techelevator/auctions/controller/AuctionController.java
@@ -2,7 +2,12 @@ package com.techelevator.auctions.controller;
 
 import com.techelevator.auctions.dao.AuctionDao;
 import com.techelevator.auctions.dao.MemoryAuctionDao;
+import com.techelevator.auctions.model.Auction;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/auctions")
@@ -13,5 +18,56 @@ public class AuctionController {
     public AuctionController() {
         this.dao = new MemoryAuctionDao();
     }
+
+//    @RequestMapping(method = RequestMethod.GET)
+//    public List<Auction> list() {
+//
+//        return dao.list();
+//    }
+
+    @RequestMapping(path = "/{id}", method = RequestMethod.GET)
+    public Auction get(@PathVariable int id) {
+        return dao.get(id);
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public Auction newAuction(@RequestBody Auction auction) {
+        return dao.create(auction);
+    }
+
+//    @RequestMapping( method = RequestMethod.GET)
+//    public List<Auction> filterByTitle(@RequestParam(value="title_like", defaultValue = "") String title) {
+//       // List<Auction> filteredAuctions = new ArrayList<>();
+//
+//        if (title != null){
+//            return dao.searchByTitle(title);
+//        } else {
+//           return  dao.list();
+//        }
+//
+//    }
+
+    @RequestMapping( method = RequestMethod.GET)
+    public List<Auction> filterByTitle(@RequestParam(value="title_like", defaultValue = "") String title, @RequestParam (value = "currentBid_lte", defaultValue = "0") double currentBid_lte ) {
+
+        if (!title.equals("") && currentBid_lte != 0){
+
+            return dao.searchByTitleAndPrice(title, currentBid_lte);
+
+        } else if (!title.equals("")){
+
+            return  dao.searchByTitle(title);
+
+        } else if (currentBid_lte != 0){
+
+            return dao.searchByPrice(currentBid_lte);
+        }
+
+        else return dao.list();
+
+    }
+
+
+
 
 }
--- java/module-2/13_Server_Side_APIs_Part_1/lecture/server/src/main/java/com/techelevator/reservations/controllers/HotelController.java
+++ java/module-2/13_Server_Side_APIs_Part_1/lecture/server/src/main/java/com/techelevator/reservations/controllers/HotelController.java
@@ -5,10 +5,13 @@ import com.techelevator.reservations.dao.MemoryHotelDao;
 import com.techelevator.reservations.dao.MemoryReservationDao;
 import com.techelevator.reservations.dao.ReservationDao;
 import com.techelevator.reservations.model.Hotel;
+import com.techelevator.reservations.model.Reservation;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
+@RestController
 public class HotelController {
 
     private HotelDao hotelDao;
@@ -39,5 +42,80 @@ public class HotelController {
     public Hotel get(@PathVariable int id) {
         return hotelDao.get(id);
     }
+
+    /**
+    * List all reservations
+     * @return a list of all the reservations in the system
+    */
+
+    @RequestMapping(path = "/reservations", method = RequestMethod.GET)
+    public List<Reservation> listReservation(){
+
+        return reservationDao.findAll();
+    }
+
+/**
+ * return reservation by ID
+ * @param id of the reservation
+ * @return all of the info of the reservation
+ */
+
+    @RequestMapping(path = "/reservations/{id}", method = RequestMethod.GET)
+    public Reservation getReservation (@PathVariable int id) {
+        return reservationDao.get(id);
+    }
+
+
+    /**
+     * List reservations by hotel
+     * @param id of the hotel
+     * @return a list of all reservations by hotel
+     */
+
+    @RequestMapping(path = "/hotels/{id}/reservations", method = RequestMethod.GET)
+    public List<Reservation> listReservationByHotel (@PathVariable int id){
+        return reservationDao.findByHotel(id);
+    }
+
+    /**
+     * add a new reservation to a hotel
+     *
+     * @body reservation object
+     * @return the created reservation
+     */
+
+    @RequestMapping(path= "/reservations", method = RequestMethod.POST)
+    public Reservation newReservation(@RequestBody Reservation reservation){
+        return reservationDao.create(reservation, reservation.getHotelID());
+    }
+
+    /**
+     * @param state the state to filter by
+     * @param city cuty to filter by
+     * @return a list of hotels that match the city and state
+     *
+     */
+
+    @RequestMapping(path= "/filter", method = RequestMethod.GET)
+    public List<Hotel> filterByCityState (@RequestParam String state, @RequestParam(required = false) String city) {
+        List<Hotel> filteredHotels = new ArrayList<>();
+        List<Hotel> allHotels = list();
+
+        for (Hotel hotel : allHotels) {
+            if (city != null) {
+                if (hotel.getAddress().getCity().toLowerCase().equals(city.toLowerCase())) {
+                    filteredHotels.add(hotel);
+                }
+            } else {
+                if (hotel.getAddress().getState().toLowerCase().equals(state.toLowerCase())) {
+                    filteredHotels.add(hotel);
+                }
+            }
+
+        }
+        return filteredHotels;
+    }
+
+
 
 }
